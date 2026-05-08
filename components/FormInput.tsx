4e8ee26f8c35217b1afc13e2682f1f84d7e93a44
Rafael Andrade <rafael_asc@outlook.com>
2026-05-08 18:49:47 -0300

    refactor: improve form input component

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -2,15 +2,33 @@ interface FormInputProps {
   type: string;
   placeholder: string;
   name: string;
+  label?: string;
 }
 
-export function FormInput({ type, placeholder, name }: FormInputProps) {
+export function FormInput({
+  type,
+  placeholder,
+  name,
+  label,
+}: FormInputProps) {
   return (
-    <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      className="w-full rounded-xl border border-white/10 bg-black/30 px-5 py-4 outline-none transition focus:border-amber-300"
-    />
+    <div>
+      {label && (
+        <label
+          htmlFor={name}
+          className="mb-2 block text-sm font-semibold text-zinc-300"
+        >
+          {label}
+        </label>
+      )}
+
+      <input
+        id={name}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        className="w-full rounded-xl border border-white/10 bg-black/30 px-5 py-4 outline-none transition focus:border-amber-300"
+      />
+    </div>
   );
 }
